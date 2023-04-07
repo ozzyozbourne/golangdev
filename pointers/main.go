a10404d8e9f00e7fbe2d427018228d446ac0a977
ozzy ozbourne <khanosaid726@gmail.com>
2023-04-08 05:25:17 +0530

    just one fix

--- pointers/main.go
+++ pointers/main.go
@@ -1,1 +1,18 @@
-package pointers
+package main
+
+import "fmt"
+
+type s struct {
+	a []string
+	b int
+}
+
+func main() {
+	fmt.Println(nakedReturn())
+	fmt.Printf("str2: %q\n", "string")
+
+}
+
+func nakedReturn() (m int) {
+	return
+}
